c44c583139b1b28cb334c54679ff052622a6a690
Miguel Bernabeu <miguel.bernabeu@devex.com>
2016-10-13 16:19:45 +0200

    Streamlined and broke into a function

--- elasticsearch_client.go
+++ elasticsearch_client.go
@@ -9,6 +9,21 @@ import (
 	"gopkg.in/olivere/elastic.v1"
 )
 
+func updateSettings(client *elastic.Client, h *http.Client, url string, setting string) (retcode int, err error) {
+	req, err := client.NewRequest("PUT", url)
+	if err != nil {
+		return
+	}
+	req.SetBodyString(setting)
+	res, err := h.Do((*http.Request)(req))
+	if err != nil {
+		return
+	}
+	defer res.Body.Close()
+	retcode = res.StatusCode
+	return
+}
+
 func main() {
 	// Obtain a client and connect to the default Elasticsearch installation
 	// on 127.0.0.1:9200. Of course you can configure your client to connect
@@ -28,26 +43,14 @@ func main() {
 	}
 	fmt.Printf("Elasticsearch version %s\n", esversion)
 
-	// Use the IndexExists service to check if a specified index exists.
-	exists, err := client.IndexExists("_settings").Do()
+	ret, err := updateSettings(client, h, "/_all/_settings", "auto_expand_replicas: 1-all")
 	if err != nil {
 		// Handle error
 		panic(err)
 	}
-	if exists {
-		fmt.Println("It exists")
+	if ret != 200 {
+		fmt.Printf("Error! %d", ret)
 	} else {
-		fmt.Println("It doesn't exist")
+		fmt.Println("All good!")
 	}
-	url := "/_all/_settings"
-	req, err := client.NewRequest("PUT", url)
-	if err != nil {
-		panic(err)
-	}
-	req.SetBodyString("auto_expand_replicas: 1-all")
-	_, err = h.Do((*http.Request)(req))
-	if err != nil {
-		panic(err)
-	}
-
 }
