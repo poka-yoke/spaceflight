40fcc0f0c17b78f786b166cfbfc0f4d0852a7269
Miguel Bernabeu <miguel.bernabeu@devex.com>
2016-10-18 17:04:12 +0200

    Fix handling of output and passing input to go binary

--- main.js
+++ main.js
@@ -1,14 +1,21 @@
 var child_process = require('child_process');
 
 exports.handler = function(event, context) {
-  var proc = child_process.spawn('./elasticsearch_client', [ JSON.stringify(event) ], { stdio: 'inherit' });
+    var proc = child_process.spawn('./elasticsearch_client', [ JSON.stringify(event) ]/*, { stdio: 'inherit' }*/);
+    
+    proc.stdout.on('data', function (data) {    // stdout handler
+  	    console.log('stdout: ' + data);
+        context.succeed('stdout: ' + data);
+	  });
 
-  proc.on('close', function(code) {
-    if(code !== 0) {
-      return context.done(new Error("Process exited with non-zero status code"));
-    }
+    proc.stderr.on('data', function (data) {	// stderr handler
+	      console.log('stderr: ' + data);
+        context.succeed('stderr: ' + data);
+        // context.fail('Something went wrong');
+    });
 
-    context.done(null);
-  });
-}
+    proc.on('exit', function (code) {
+        console.log('lambda exited with code ' + code);
+    });
+};
 
