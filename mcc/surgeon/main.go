742ab7cfedf54bc4ea2da0222d3881a67f39bf63
Ignasi Fosch <natx@y10k.ws>
2016-11-23 12:34:53 +0100

    Adds printing to ProcessList

--- mcc/surgeon/main.go
+++ mcc/surgeon/main.go
@@ -7,7 +7,6 @@ import (
 	_ "github.com/lib/pq"
 	"github.com/poka-yoke/spaceflight/mcc/surgeon/surgeon"
 	"log"
-	"sort"
 	"strings"
 )
 
@@ -25,37 +24,24 @@ func main() {
 		"connection",
 		"postgres://localhost/postgres",
 		"Connection string: "+
-			"'postgres://[user[:password]@]hostname[:port]/database",
+			"'postgres://[user[:password]@]hostname[:port]/"+
+			"database",
 	)
 	showInactive := flag.Bool("i", false, "Show inactive processes")
 
 	flag.Parse()
+	options := map[string]bool{
+		"showInactive": *showInactive,
+	}
 
 	db, err := GetDB(*connection)
 	defer db.Close()
 	if err != nil {
 		log.Panic(err.Error())
 	}
-	ps, err := surgeon.GetProcessLists(db)
+	result, err := surgeon.GetProcessLists(db, options)
 	if err != nil {
-		fmt.Print("In surgeon.GetProcessLists: ")
 		log.Panic(err.Error())
 	}
-	fmt.Printf(" %5s %10s %19s %17s %13s %9s %s \n",
-		"PID", "USER", "DB", "CLIENT", "DURATION", "WAITING", "QUERY")
-	sort.Sort(ps)
-	for _, process := range ps {
-		if *showInactive || process.IsActive() {
-			fmt.Printf(
-				" %5d %12s %20s %17s %10.2f %9s %s \n",
-				process.Pid,
-				process.Usename,
-				process.Datname,
-				process.Client.String(),
-				process.Duration.Duration,
-				process.Waiting,
-				process.Query,
-			)
-		}
-	}
+	fmt.Print(result)
 }
