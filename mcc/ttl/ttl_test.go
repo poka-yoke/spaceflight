3517ed9de58c9ebf1ea3261399a68e4cc628100c
Miguel Bernabeu <miguel.bernabeu@devex.com>
2016-12-05 22:30:48 +0100

    Add mocking for Route53

--- mcc/ttl/ttl_test.go
+++ mcc/ttl/ttl_test.go
@@ -5,13 +5,15 @@ import (
 	"testing"
 
 	"github.com/aws/aws-sdk-go/service/route53"
+	"github.com/aws/aws-sdk-go/service/route53/route53iface"
 )
 
 var one = "one.example.com"
 var two = "two.example.com"
-
+var hundred = "100"
 var A = "A"
 var AAAA = "AAAA"
+var fals = false
 var duration1 int64 = 1
 var duration5 int64 = 5
 
@@ -30,6 +32,22 @@ var tworecordAAAA = &route53.ResourceRecordSet{
 var ResourceRecordSetList = []*route53.ResourceRecordSet{
 	onerecordA,
 	tworecordAAAA,
+}
+
+// Define a mock struct to be used in unit tests.
+type mockRoute53Client struct {
+	route53iface.Route53API
+}
+
+func (m *mockRoute53Client) ListResourceRecordSets(
+	params *route53.ListResourceRecordSetsInput,
+) (out *route53.ListResourceRecordSetsOutput, err error) {
+	out = &route53.ListResourceRecordSetsOutput{
+		IsTruncated:        &fals,
+		MaxItems:           &hundred,
+		ResourceRecordSets: ResourceRecordSetList,
+	}
+	return
 }
 
 var rrstest = []struct {
