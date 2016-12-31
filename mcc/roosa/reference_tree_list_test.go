3cc5cc6ca8494da6f1e3e91c8a145d3ec7767a50
Ignasi Fosch <natx@y10k.ws>
2016-12-31 17:03:52 +0100

    Adds tests for ReferenceTreeList

--- mcc/roosa/reference_tree_list_test.go
+++ mcc/roosa/reference_tree_list_test.go
@@ -1,95 +1,76 @@
 package roosa
 
 import (
+	"fmt"
+	"strings"
 	"testing"
+
+	"github.com/aws/aws-sdk-go/service/route53"
 )
 
-func TestNewReferenceTreeList(t *testing.T) {
-	// 	records []*route53.ResourceRecordSet,
-	// ) *ReferenceTreeList {
-	// 	return &ReferenceTreeList{
-	// 		records: ttl.FilterResourceRecordSetType(
-	// 			records,
-	// 			recordTypes,
-	// 		),
-	// 		lookup: nil,
-	// 	}
+var recordsContents = []string{
+	"example.com. SOA admin.example.com 2017010101 86400 7200 604800 300",
+	"example.com. A 10.10.10.10",
+	"root.example.com. A 127.0.0.1",
+	"root-son.example.com. CNAME root.example.com",
+	"root-son-sibling.example.com. CNAME root.example.com",
+	"service1.example.com. CNAME root.example.com",
+	"service1.example.com. CNAME root2.example.com",
+	"root-grandson.example.com. CNAME root-son.example.com",
+	"root2.example.com. A 127.0.0.2",
+	"test.example.com. CNAME test.example2.com",
+	"multiple-a.example.com. A 127.0.0.1,127.0.0.2,127.0.0.3",
+}
+
+var expectedOutputContents = []string{
+	"root2.example.com. A 127.0.0.2",
+	"\tservice1.example.com. CNAME root2.example.com",
+	"test.example.com. CNAME test.example2.com",
+	"multiple-a.example.com. A 127.0.0.1, 127.0.0.2, 127.0.0.3",
+	"example.com. A 10.10.10.10",
+	"root.example.com. A 127.0.0.1",
+	"\troot-son-sibling.example.com. CNAME root.example.com",
+	"\troot-son.example.com. CNAME root.example.com",
+	"\t\troot-grandson.example.com. CNAME root-son.example.com",
+	"\tservice1.example.com. CNAME root.example.com",
+	"",
+}
+
+func generateRoute53RRS() (records []*route53.ResourceRecordSet) {
+	records = []*route53.ResourceRecordSet{}
+	for _, record := range recordsContents {
+		recordValues := strings.Fields(record)
+		values := strings.Split(recordValues[2], ",")
+		rrs := []*route53.ResourceRecord{}
+		for _, value := range values {
+			newValue := value
+			rr := &route53.ResourceRecord{Value: &newValue}
+			rrs = append(rrs, rr)
+		}
+		route53RRS := &route53.ResourceRecordSet{
+			Name:            &recordValues[0],
+			Type:            &recordValues[1],
+			ResourceRecords: rrs,
+		}
+		records = append(records, route53RRS)
+	}
+	return
 }
 
 func TestReferenceTreeListString(t *testing.T) {
-	// 	if rtl.lookup == nil {
-	// 		rtl.GetReferenceTrees()
-	// 	}
-	// 	output = ""
-	// 	for _, tree := range rtl.lookup {
-	// 		for _, node := range tree {
-	// 			output += fmt.Sprintf("%v\n", node)
-	// 		}
-	// 	}
-	// 	return
-}
-
-func TestReferenceTreeListFill(t *testing.T) {
-	// 	rtl.lookup = map[string][]*Node{}
-	// 	for _, val := range rtl.records {
-	// 		node := &Node{
-	// 			content: val,
-	// 		}
-	// 		name := strings.TrimSuffix(*val.Name, ".")
-	// 		rtl.lookup[name] = append(rtl.lookup[name], node)
-	// 	}
-	// 	log.Printf("Added %d records to Lookup\n", len(rtl.lookup))
-	// 	return
-}
-
-func TestReferenceTreeListClean(t *testing.T) {
-	// 	for name, nodes := range rtl.lookup {
-	// 		for _, node := range nodes {
-	// 			if !node.IsRoot() {
-	// 				delete(rtl.lookup, name)
-	// 			}
-	// 		}
-	// 	}
-}
-
-func TestReferenceTreeListCompact(t *testing.T) {
-	// 	for name, nodes := range rtl.lookup {
-	// 		for _, node := range nodes {
-	// 			value := *node.content.ResourceRecords[0].Value
-	// 			if *node.content.Type == "CNAME" {
-	// 				if parents, ok := rtl.lookup[value]; ok {
-	// 					for _, parent := range parents {
-	// 						log.Printf(
-	// 							"%v (%v) has Parent %v\n",
-	// 							name,
-	// 							value,
-	// 							*parent.content.Name,
-	// 						)
-	// 						parent.children = append(
-	// 							parent.children,
-	// 							node,
-	// 						)
-	// 						node.parent = parent
-	// 					}
-	// 				} else {
-	// 					log.Printf(
-	// 						"%v (%v) is Out of domain\n",
-	// 						name,
-	// 						value,
-	// 					)
-	// 				}
-	// 			} else {
-	// 				log.Printf(
-	// 					"%v (%v) is Root\n",
-	// 					name,
-	// 					value,
-	// 				)
-	// 			}
-	// 		}
-	// 	}
-	// 	rtl.clean()
-	// 	log.Printf(
-	// 		"Cleared children, %d records left in Lookup\n",
-	// 		len(rtl.lookup),
-	// 	)
+	rtl := NewReferenceTreeList(generateRoute53RRS())
+	output := fmt.Sprintf("%v", rtl)
+	for _, line := range expectedOutputContents {
+		if !strings.Contains(output, line) {
+			t.Errorf("'%v' should be in output:\n%v", line, output)
+		}
+	}
+	outputLines := strings.Split(output, "\n")
+	if len(outputLines) != len(expectedOutputContents) {
+		t.Errorf(
+			"Output line count (%d) doesn't match with expected (%d)",
+			len(outputLines),
+			len(expectedOutputContents),
+		)
+	}
 }
