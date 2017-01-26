c9bd4d1939802d9077401388209c40a7b0ba6891
Miguel Bernabeu <miguel.bernabeu@devex.com>
2017-01-26 23:58:05 +0100

    Add ttl-equivalent behaviour in ttl subcommand

--- mcc/got/cmd/ttl.go
+++ mcc/got/cmd/ttl.go
@@ -1,24 +1,104 @@
 package cmd
 
 import (
-	"fmt"
+	"log"
 
+	"github.com/aws/aws-sdk-go/service/route53"
 	"github.com/spf13/cobra"
+
+	"github.com/Devex/spaceflight/mcc/got/got"
 )
+
+var dryrun, exclude, wait, filterByName, filterByType bool
+var zoneName string
+var ttl int64
 
 // ttlCmd represents the ttl command
 var ttlCmd = &cobra.Command{
-	Use:   "ttl",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Use:   "ttl [flags] [filters ...]",
+	Short: "Modify Time To Live of a set of records in a DNS zone",
+	Long:  ``,
 	Run: func(cmd *cobra.Command, args []string) {
-		// TODO: Work your own magic here
-		fmt.Println("ttl called")
+		svc := got.Init()
+		if len(zoneName) <= 0 {
+			log.Fatal("No zone name specified")
+		}
+		zoneID := got.GetZoneID(zoneName, svc)
+
+		list := got.GetResourceRecordSet(zoneID, svc)
+		// Filter list in between
+		if exclude && filterByType {
+			list = got.FilterResourceRecords(
+				list,
+				args,
+				func(
+					elem *route53.ResourceRecordSet,
+					filter string,
+				) *route53.ResourceRecordSet {
+					if *elem.Type != filter {
+						return elem
+					}
+					return nil
+				},
+			)
+		} else if exclude && filterByName {
+			list = got.FilterResourceRecords(
+				list,
+				args,
+				func(
+					elem *route53.ResourceRecordSet,
+					filter string,
+				) *route53.ResourceRecordSet {
+					if *elem.Name != filter {
+						return elem
+					}
+					return nil
+				},
+			)
+		} else if filterByType {
+			list = got.FilterResourceRecords(
+				list,
+				args,
+				func(
+					elem *route53.ResourceRecordSet,
+					filter string,
+				) *route53.ResourceRecordSet {
+					if *elem.Type == filter {
+						return elem
+					}
+					return nil
+				},
+			)
+		} else if filterByName {
+			list = got.FilterResourceRecords(
+				list,
+				args,
+				func(
+					elem *route53.ResourceRecordSet,
+					filter string,
+				) *route53.ResourceRecordSet {
+					if *elem.Name == filter {
+						return elem
+					}
+					return nil
+				},
+			)
+		}
+		changeResponse, err := got.UpsertResourceRecordSetTTL(
+			list,
+			ttl,
+			&zoneID,
+			svc,
+		)
+		if err != nil {
+			log.Panic(err.Error())
+		}
+		if wait && !dryrun {
+			got.WaitForChangeToComplete(
+				changeResponse.ChangeInfo,
+				svc,
+			)
+		}
 	},
 }
 
@@ -30,6 +110,55 @@ func init() {
 	// Cobra supports Persistent Flags which will work for this command
 	// and all subcommands, e.g.:
 	// ttlCmd.PersistentFlags().String("foo", "", "A help for foo")
+	ttlCmd.PersistentFlags().BoolVarP(
+		&dryrun,
+		"dryrun",
+		"",
+		false,
+		"Don't really do anything",
+	)
+	ttlCmd.PersistentFlags().BoolVarP(
+		&exclude,
+		"exclude",
+		"",
+		false,
+		"Exclude records matching list",
+	)
+	ttlCmd.PersistentFlags().BoolVarP(
+		&wait,
+		"wait",
+		"",
+		false,
+		"Don't return until operation is completed",
+	)
+	ttlCmd.PersistentFlags().StringVarP(
+		&zoneName,
+		"zone",
+		"",
+		"",
+		"Name of the zone to work on.",
+	)
+	ttlCmd.PersistentFlags().Int64VarP(
+		&ttl,
+		"ttl",
+		"",
+		30,
+		"New TTL value",
+	)
+	ttlCmd.PersistentFlags().BoolVarP(
+		&filterByName,
+		"name",
+		"n",
+		false,
+		"Filters are to be used as names",
+	)
+	ttlCmd.PersistentFlags().BoolVarP(
+		&filterByType,
+		"type",
+		"t",
+		false,
+		"Filters are to be used as types",
+	)
 
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
