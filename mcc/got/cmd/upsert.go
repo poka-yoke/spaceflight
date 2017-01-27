88f25a213fd51c70b454e742b2f7f12cd2170792
Miguel Bernabeu <miguel.bernabeu@devex.com>
2017-01-27 09:34:32 +0100

    Add upsert subcommand to modify properties of records

--- mcc/got/cmd/upsert.go
+++ mcc/got/cmd/upsert.go
@@ -1,24 +1,44 @@
 package cmd
 
 import (
-	"fmt"
+	"log"
 
 	"github.com/spf13/cobra"
+
+	"github.com/Devex/spaceflight/mcc/got/got"
 )
+
+var name, typ string
 
 // upsertCmd represents the upsert command
 var upsertCmd = &cobra.Command{
 	Use:   "upsert",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "upsert [flags] <destination>",
+	Long:  ``,
 	Run: func(cmd *cobra.Command, args []string) {
-		// TODO: Work your own magic here
-		fmt.Println("upsert called")
+		svc := got.Init()
+		if len(zoneName) <= 0 {
+			log.Fatal("No zone name specified")
+		}
+		if len(name) <= 0 {
+			log.Fatal("No record name specified")
+		}
+		if len(typ) <= 0 {
+			log.Fatal("No record type specified")
+		}
+		if len(args) <= 0 {
+			log.Fatal("No destination specified")
+		}
+		zoneid := got.GetZoneID(zoneName, svc)
+		list := got.NewResourceRecordList(args)
+		changes := got.UpsertChangeListNames(list, ttl, name, typ)
+		if !dryrun {
+			res, err := got.ApplyChanges(changes, &zoneid, svc)
+			if err != nil {
+				log.Fatal(err.Error())
+			}
+			log.Println(res)
+		}
 	},
 }
 
@@ -30,6 +50,55 @@ func init() {
 	// Cobra supports Persistent Flags which will work for this command
 	// and all subcommands, e.g.:
 	// upsertCmd.PersistentFlags().String("foo", "", "A help for foo")
+	upsertCmd.PersistentFlags().BoolVarP(
+		&dryrun,
+		"dryrun",
+		"",
+		false,
+		"Don't really do anything",
+	)
+	upsertCmd.PersistentFlags().BoolVarP(
+		&exclude,
+		"exclude",
+		"",
+		false,
+		"Exclude records matching list",
+	)
+	upsertCmd.PersistentFlags().BoolVarP(
+		&wait,
+		"wait",
+		"",
+		false,
+		"Don't return until operation is completed",
+	)
+	upsertCmd.PersistentFlags().StringVarP(
+		&zoneName,
+		"zone",
+		"",
+		"",
+		"Name of the zone to work on.",
+	)
+	upsertCmd.PersistentFlags().Int64VarP(
+		&ttl,
+		"ttl",
+		"",
+		30,
+		"New UPSERT value",
+	)
+	upsertCmd.PersistentFlags().StringVarP(
+		&name,
+		"name",
+		"",
+		"",
+		"Name of the record to upsert.",
+	)
+	upsertCmd.PersistentFlags().StringVarP(
+		&typ,
+		"type",
+		"",
+		"",
+		"Type of the record to upsert.",
+	)
 
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
