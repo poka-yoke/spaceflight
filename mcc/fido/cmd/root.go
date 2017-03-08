6e3654abf9bacb356c0c791b4dc2471b0e6cc40d
Miguel Bernabeu <miguel.bernabeu@devex.com>
2017-03-08 10:53:41 +0100

    Remove some boilerplate

--- mcc/fido/cmd/root.go
+++ mcc/fido/cmd/root.go
@@ -13,16 +13,8 @@ var cfgFile string
 // RootCmd represents the base command when called without any subcommands
 var RootCmd = &cobra.Command{
 	Use:   "fido",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	//	Run: func(cmd *cobra.Command, args []string) { },
+	Short: "Manage CustomJSON from OpsWorks",
+	Long:  ``,
 }
 
 // Execute adds all child commands to the root command sets flags appropriately.
@@ -44,7 +36,6 @@ func init() {
 	RootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.fido.yaml)")
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
-	RootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
 
 // initConfig reads in config file and ENV variables if set.
