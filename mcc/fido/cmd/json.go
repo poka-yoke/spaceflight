409e0c17e40d07e17cec7950068c1e6f6ef17aad
Miguel Bernabeu <miguel.bernabeu@devex.com>
2017-03-08 10:53:41 +0100

    Remove some boilerplate

--- mcc/fido/cmd/json.go
+++ mcc/fido/cmd/json.go
@@ -9,15 +9,9 @@ import (
 // jsonCmd represents the json command
 var jsonCmd = &cobra.Command{
 	Use:   "json",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Modify contents of supplied JSON file",
+	Long:  ``,
 	Run: func(cmd *cobra.Command, args []string) {
-		// TODO: Work your own magic here
 		fmt.Println("json called")
 	},
 }
