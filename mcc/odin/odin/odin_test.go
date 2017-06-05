c24debac970cb25b2c0803409960db56440479f8
Ignasi Fosch <natx@y10k.ws>
2017-06-05 18:20:54 +0200

    DVX-5064: Adds odin to create RDS instances

---
+++ mcc/odin/odin/odin_test.go
@@ -0,0 +1,226 @@
+package odin
+
+import (
+	"errors"
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/rds"
+	"github.com/aws/aws-sdk-go/service/rds/rdsiface"
+)
+
+type mockRDSClient struct {
+	rdsiface.RDSAPI
+	dbInstancesEndpoints map[string]rds.Endpoint
+}
+
+// DescribeDBInstances mocks rds.DescribeDBInstances.
+func (m mockRDSClient) DescribeDBInstances(
+	describeParams *rds.DescribeDBInstancesInput,
+) (
+	result *rds.DescribeDBInstancesOutput,
+	err error,
+) {
+	status := "available"
+	endpoint, _ := m.dbInstancesEndpoints[*describeParams.DBInstanceIdentifier]
+	result = &rds.DescribeDBInstancesOutput{
+		DBInstances: []*rds.DBInstance{
+			{
+				DBInstanceIdentifier: describeParams.DBInstanceIdentifier,
+				DBInstanceStatus:     &status,
+				Endpoint:             &endpoint,
+			},
+		},
+	}
+	return
+}
+
+// CreateDBInstance mocks rds.CreateDBInstance.
+func (m mockRDSClient) CreateDBInstance(
+	inputParams *rds.CreateDBInstanceInput,
+) (
+	result *rds.CreateDBInstanceOutput,
+	err error,
+) {
+	if err = inputParams.Validate(); err != nil {
+		return
+	}
+	az := "us-east-1c"
+	if inputParams.AvailabilityZone != nil {
+		az = *inputParams.AvailabilityZone
+	}
+	if inputParams.MasterUsername == nil ||
+		*inputParams.MasterUsername == "" {
+		err = errors.New("Specify Master User")
+		return
+	}
+	if inputParams.MasterUserPassword == nil ||
+		*inputParams.MasterUserPassword == "" {
+		err = errors.New("Specify Master User Password")
+		return
+	}
+	if inputParams.AllocatedStorage == nil ||
+		*inputParams.AllocatedStorage < 5 ||
+		*inputParams.AllocatedStorage > 6144 {
+		err = errors.New("Specify size between 5 and 6144")
+		return
+	}
+	region := az[:len(az)-1]
+	endpoint := fmt.Sprintf(
+		"%s.0.%s.rds.amazonaws.com",
+		*inputParams.DBInstanceIdentifier,
+		region,
+	)
+	port := int64(5432)
+	m.dbInstancesEndpoints[*inputParams.DBInstanceIdentifier] = rds.Endpoint{
+		Address: &endpoint,
+		Port:    &port,
+	}
+	status := "creating"
+	result = &rds.CreateDBInstanceOutput{
+		DBInstance: &rds.DBInstance{
+			AllocatedStorage: inputParams.AllocatedStorage,
+			DBInstanceArn: aws.String(
+				fmt.Sprintf(
+					"arn:aws:rds:%s:0:db:%s",
+					region,
+					inputParams.DBInstanceIdentifier,
+				),
+			),
+			DBInstanceIdentifier: inputParams.DBInstanceIdentifier,
+			DBInstanceStatus:     &status,
+			Engine:               inputParams.Engine,
+		},
+	}
+	return
+}
+
+type createDBInstanceCase struct {
+	name               string
+	identifier         string
+	instanceType       string
+	masterUserPassword string
+	masterUser         string
+	size               int64
+	endpoint           string
+	expectedError      string
+}
+
+var createDBInstanceCases = []createDBInstanceCase{
+	{
+		name:               "Creating simple instance",
+		identifier:         "test1",
+		instanceType:       "db.m1.small",
+		masterUser:         "master",
+		masterUserPassword: "master",
+		size:               5,
+		endpoint:           "test1.0.us-east-1.rds.amazonaws.com",
+		expectedError:      "",
+	},
+	{
+		name:               "Fail because non-present user",
+		identifier:         "test1",
+		instanceType:       "db.m1.small",
+		masterUserPassword: "master",
+		size:               5,
+		endpoint:           "",
+		expectedError:      "Specify Master User",
+	},
+	{
+		name:               "Fail because empty user",
+		identifier:         "test1",
+		instanceType:       "db.m1.small",
+		masterUser:         "",
+		masterUserPassword: "master",
+		size:               5,
+		endpoint:           "",
+		expectedError:      "Specify Master User",
+	},
+	{
+		name:          "Fail because non-present password",
+		identifier:    "test1",
+		instanceType:  "db.m1.small",
+		masterUser:    "master",
+		size:          5,
+		endpoint:      "",
+		expectedError: "Specify Master User Password",
+	},
+	{
+		name:               "Fail because empty password",
+		identifier:         "test1",
+		instanceType:       "db.m1.small",
+		masterUser:         "master",
+		masterUserPassword: "",
+		size:               5,
+		endpoint:           "",
+		expectedError:      "Specify Master User Password",
+	},
+	{
+		name:               "Fail because non-present size",
+		identifier:         "test1",
+		instanceType:       "db.m1.small",
+		masterUser:         "master",
+		masterUserPassword: "master",
+		endpoint:           "",
+		expectedError:      "Specify size between 5 and 6144",
+	},
+	{
+		name:               "Fail because too small size",
+		identifier:         "test1",
+		instanceType:       "db.m1.small",
+		masterUser:         "master",
+		masterUserPassword: "master",
+		size:               4,
+		endpoint:           "",
+		expectedError:      "Specify size between 5 and 6144",
+	},
+	{
+		name:               "Fail because too big size",
+		identifier:         "test1",
+		instanceType:       "db.m1.small",
+		masterUser:         "master",
+		masterUserPassword: "master",
+		size:               6145,
+		endpoint:           "",
+		expectedError:      "Specify size between 5 and 6144",
+	},
+}
+
+func TestCreateDB(t *testing.T) {
+	svc := &mockRDSClient{
+		dbInstancesEndpoints: map[string]rds.Endpoint{},
+	}
+	duration = time.Duration(0)
+	for _, useCase := range createDBInstanceCases {
+		t.Run(
+			useCase.name,
+			func(t *testing.T) {
+				endpoint, err := CreateDBInstance(
+					useCase.identifier,
+					useCase.instanceType,
+					useCase.masterUser,
+					useCase.masterUserPassword,
+					useCase.size,
+					svc,
+				)
+				if err != nil {
+					if fmt.Sprintf("%s", err) != useCase.expectedError {
+						t.Errorf(
+							"Unexpected error %s",
+							err,
+						)
+					}
+				}
+				if endpoint != useCase.endpoint {
+					t.Errorf(
+						"Unexpected output: %s should be %s",
+						endpoint,
+						useCase.endpoint,
+					)
+				}
+			},
+		)
+	}
+}
