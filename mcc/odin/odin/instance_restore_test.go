eee85b791ac973724c27d961d0641ba0ee43bd7d
Ignasi Fosch <natx@y10k.ws>
2017-08-18 15:21:20 +0200

    Move restore tests to instance_restore_test.go

--- mcc/odin/odin/instance_restore_test.go
+++ mcc/odin/odin/instance_restore_test.go
@@ -2,6 +2,7 @@ package odin_test
 
 import (
 	"testing"
+	"time"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/rds"
@@ -93,6 +94,95 @@ func TestGetRestoreDBInstanceFromDBSnapshotInput(t *testing.T) {
 							"Unexpected output: %s should be %s",
 							restoreSnapshotInput,
 							useCase.expectedRestoreSnapshotInput,
+						)
+					}
+				}
+			},
+		)
+	}
+}
+
+type restoreInstanceCase struct {
+	name                 string
+	identifier           string
+	instanceType         string
+	masterUserPassword   string
+	masterUser           string
+	size                 int64
+	originalInstanceName string
+	endpoint             string
+	expectedError        string
+	snapshot             *rds.DBSnapshot
+}
+
+var restoreInstanceCases = []restoreInstanceCase{
+	// Uses snapshot to restore from
+	{
+		name:                 "Uses snapshot to restore from",
+		identifier:           "test1",
+		instanceType:         "db.m1.small",
+		masterUser:           "master",
+		masterUserPassword:   "master",
+		size:                 6144,
+		originalInstanceName: "production",
+		endpoint:             "test1.0.us-east-1.rds.amazonaws.com",
+		expectedError:        "",
+		snapshot:             exampleSnapshot1,
+	},
+	// Uses non existing snapshot to restore from
+	{
+		name:                 "Uses non existing snapshot to restore from",
+		identifier:           "test1",
+		instanceType:         "db.m1.small",
+		masterUser:           "master",
+		masterUserPassword:   "master",
+		size:                 6144,
+		originalInstanceName: "develop",
+		endpoint:             "",
+		expectedError:        "Couldn't find snapshot for develop instance",
+		snapshot:             exampleSnapshot1,
+	},
+}
+
+func TestRestoreInstance(t *testing.T) {
+	svc := newMockRDSClient()
+	odin.Duration = time.Duration(0)
+	for _, useCase := range restoreInstanceCases {
+		t.Run(
+			useCase.name,
+			func(t *testing.T) {
+				if useCase.originalInstanceName != "" {
+					svc.dbSnapshots[*useCase.snapshot.DBInstanceIdentifier] = []*rds.DBSnapshot{
+						useCase.snapshot,
+					}
+				}
+				params := odin.RestoreParams{
+					InstanceType:         useCase.instanceType,
+					OriginalInstanceName: useCase.originalInstanceName,
+				}
+				endpoint, err := odin.RestoreInstance(
+					useCase.identifier,
+					params,
+					svc,
+				)
+				if err != nil {
+					if err.Error() != useCase.expectedError {
+						t.Errorf(
+							"Unexpected error %s",
+							err,
+						)
+					}
+				} else if useCase.expectedError != "" {
+					t.Errorf(
+						"Expected error %s didn't happened",
+						useCase.expectedError,
+					)
+				} else {
+					if endpoint != useCase.endpoint {
+						t.Errorf(
+							"Unexpected output: %s should be %s",
+							endpoint,
+							useCase.endpoint,
 						)
 					}
 				}
