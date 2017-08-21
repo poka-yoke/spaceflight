07562fedecc73f799e3b999237f3727dbd6b79cc
Ignasi Fosch <natx@y10k.ws>
2017-08-22 00:13:35 +0200

    Improve instance_create_test.go

--- mcc/odin/odin/instance_create_test.go
+++ mcc/odin/odin/instance_create_test.go
@@ -4,123 +4,130 @@ import (
 	"testing"
 	"time"
 
+	"github.com/go-test/deep"
+
 	"github.com/poka-yoke/spaceflight/mcc/odin/odin"
 )
 
 type createInstanceCase struct {
-	name               string
-	identifier         string
-	instanceType       string
-	masterUserPassword string
-	masterUser         string
-	size               int64
-	endpoint           string
-	expectedError      string
+	name          string
+	identifier    string
+	instanceType  string
+	password      string
+	user          string
+	size          int64
+	expected      string
+	expectedError string
+}
+
+func (t *createInstanceCase) expectingError(err error) bool {
+	return t.expectedError != "" && err.Error() != t.expectedError
 }
 
 var createInstanceCases = []createInstanceCase{
 	// Creating simple instance
 	{
-		name:               "Creating simple instance",
-		identifier:         "test1",
-		instanceType:       "db.m1.small",
-		masterUser:         "master",
-		masterUserPassword: "master",
-		size:               5,
-		endpoint:           "test1.0.us-east-1.rds.amazonaws.com",
-		expectedError:      "",
+		name:          "Creating simple instance",
+		identifier:    "test1",
+		instanceType:  "db.m1.small",
+		user:          "master",
+		password:      "master",
+		size:          5,
+		expected:      "test1.0.us-east-1.rds.amazonaws.com",
+		expectedError: "",
 	},
 	// Fail because empty user
 	{
-		name:               "Fail because empty user",
-		identifier:         "test1",
-		instanceType:       "db.m1.small",
-		masterUser:         "",
-		masterUserPassword: "master",
-		size:               5,
-		endpoint:           "",
-		expectedError:      "Specify Master User",
+		name:          "Fail because empty user",
+		identifier:    "test1",
+		instanceType:  "db.m1.small",
+		user:          "",
+		password:      "master",
+		size:          5,
+		expected:      "",
+		expectedError: "Specify Master User",
 	},
 	// Fail because empty password
 	{
-		name:               "Fail because empty password",
-		identifier:         "test1",
-		instanceType:       "db.m1.small",
-		masterUser:         "master",
-		masterUserPassword: "",
-		size:               5,
-		endpoint:           "",
-		expectedError:      "Specify Master User Password",
+		name:          "Fail because empty password",
+		identifier:    "test1",
+		instanceType:  "db.m1.small",
+		user:          "master",
+		password:      "",
+		size:          5,
+		expected:      "",
+		expectedError: "Specify Master User Password",
 	},
 	// Fail because non-present size
 	{
-		name:               "Fail because non-present size",
-		identifier:         "test1",
-		instanceType:       "db.m1.small",
-		masterUser:         "master",
-		masterUserPassword: "master",
-		endpoint:           "",
-		expectedError:      "Specify size between 5 and 6144",
+		name:          "Fail because non-present size",
+		identifier:    "test1",
+		instanceType:  "db.m1.small",
+		user:          "master",
+		password:      "master",
+		expected:      "",
+		expectedError: "Specify size between 5 and 6144",
 	},
 	// Fail because too small size
 	{
-		name:               "Fail because too small size",
-		identifier:         "test1",
-		instanceType:       "db.m1.small",
-		masterUser:         "master",
-		masterUserPassword: "master",
-		endpoint:           "",
-		expectedError:      "Specify size between 5 and 6144",
+		name:          "Fail because too small size",
+		identifier:    "test1",
+		instanceType:  "db.m1.small",
+		user:          "master",
+		password:      "master",
+		expected:      "",
+		expectedError: "Specify size between 5 and 6144",
 	},
 	// Fail because too big size
 	{
-		name:               "Fail because too big size",
-		identifier:         "test1",
-		instanceType:       "db.m1.small",
-		masterUser:         "master",
-		masterUserPassword: "master",
-		size:               6145,
-		endpoint:           "",
-		expectedError:      "Specify size between 5 and 6144",
+		name:          "Fail because too big size",
+		identifier:    "test1",
+		instanceType:  "db.m1.small",
+		user:          "master",
+		password:      "master",
+		size:          6145,
+		expected:      "",
+		expectedError: "Specify size between 5 and 6144",
 	},
 }
 
 func TestCreateInstance(t *testing.T) {
 	svc := newMockRDSClient()
 	odin.Duration = time.Duration(0)
-	for _, useCase := range createInstanceCases {
+	for _, test := range createInstanceCases {
 		t.Run(
-			useCase.name,
+			test.name,
 			func(t *testing.T) {
 				params := odin.CreateParams{
-					InstanceType: useCase.instanceType,
-					User:         useCase.masterUser,
-					Password:     useCase.masterUserPassword,
-					Size:         useCase.size,
+					InstanceType: test.instanceType,
+					User:         test.user,
+					Password:     test.password,
+					Size:         test.size,
 				}
-				endpoint, err := odin.CreateInstance(
-					useCase.identifier,
+				actual, err := odin.CreateInstance(
+					test.identifier,
 					params,
 					svc,
 				)
-				if err != nil {
-					if err.Error() != useCase.expectedError {
-						t.Errorf(
-							"Unexpected error %s",
-							err,
-						)
-					}
-				} else if useCase.expectedError != "" {
+				switch {
+				case err != nil && test.expectingError(err):
 					t.Errorf(
-						"Expected error %s didn't happened",
-						useCase.expectedError,
+						"Unexpected error: %v",
+						err,
 					)
-				} else {
-					if endpoint != useCase.endpoint {
+				case err == nil && test.expectedError != "":
+					t.Errorf(
+						"Expected error: %v missing",
+						test.expectedError,
+					)
+				case err == nil:
+					if diff := deep.Equal(
+						actual,
+						test.expected,
+					); diff != nil {
 						t.Errorf(
-							"Unexpected output: %s should be %s",
-							endpoint,
-							useCase.endpoint,
+							"Unexpected output: %s",
+							diff,
 						)
 					}
 				}
