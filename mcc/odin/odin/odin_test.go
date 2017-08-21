3d8586b4e7787f1aef0298c6c852b7585c57385f
Ignasi Fosch <natx@y10k.ws>
2017-08-22 00:35:06 +0200

    Make testCase capable to check error and result

--- mcc/odin/odin/odin_test.go
+++ mcc/odin/odin/odin_test.go
@@ -191,8 +191,33 @@ type testCase struct {
 	expectedError string
 }
 
-func (t *testCase) expectingError(err error) bool {
-	return t.expectedError != "" && err.Error() != t.expectedError
+func (tc *testCase) expectingError(err error) bool {
+	return tc.expectedError != "" && err.Error() != tc.expectedError
+}
+
+func (tc *testCase) check(actual interface{}, err error, t *testing.T) {
+	switch {
+	case err != nil && tc.expectingError(err):
+		t.Errorf(
+			"Unexpected error: %v",
+			err,
+		)
+	case err == nil && tc.expectedError != "":
+		t.Errorf(
+			"Expected error: %v missing",
+			tc.expectedError,
+		)
+	case err == nil:
+		if diff := deep.Equal(
+			actual,
+			tc.expected,
+		); diff != nil {
+			t.Errorf(
+				"Unexpected output: %s",
+				diff,
+			)
+		}
+	}
 }
 
 var exampleSnapshot1Type = aws.String("db.m1.medium")
