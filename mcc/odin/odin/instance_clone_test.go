e4c94b676bc7b2349a719dd7d3792124e1666910
Ignasi Fosch <natx@y10k.ws>
2017-08-22 00:41:19 +0200

    Make instance_clone_test use testCase

--- mcc/odin/odin/instance_clone_test.go
+++ mcc/odin/odin/instance_clone_test.go
@@ -5,54 +5,52 @@ import (
 	"time"
 
 	"github.com/aws/aws-sdk-go/service/rds"
-	"github.com/go-test/deep"
 
 	"github.com/poka-yoke/spaceflight/mcc/odin/odin"
 )
 
 type cloneInstanceCase struct {
-	name          string
-	identifier    string
-	instanceType  string
-	password      string
-	user          string
-	size          int64
-	from          string
-	expected      string
-	expectedError string
-	snapshot      *rds.DBSnapshot
-}
-
-func (t *cloneInstanceCase) expectingError(err error) bool {
-	return t.expectedError != "" && err.Error() != t.expectedError
+	testCase
+	name         string
+	identifier   string
+	instanceType string
+	password     string
+	user         string
+	size         int64
+	from         string
+	snapshot     *rds.DBSnapshot
 }
 
 var cloneInstanceCases = []cloneInstanceCase{
 	// Uses snapshot to copy from
 	{
-		name:          "Uses snapshot to copy from",
-		identifier:    "test1",
-		instanceType:  "db.m1.small",
-		user:          "master",
-		password:      "master",
-		size:          6144,
-		from:          "production",
-		expected:      "test1.0.us-east-1.rds.amazonaws.com",
-		expectedError: "",
-		snapshot:      exampleSnapshot1,
+		testCase: testCase{
+			expected:      "test1.0.us-east-1.rds.amazonaws.com",
+			expectedError: "",
+		},
+		name:         "Uses snapshot to copy from",
+		identifier:   "test1",
+		instanceType: "db.m1.small",
+		user:         "master",
+		password:     "master",
+		size:         6144,
+		from:         "production",
+		snapshot:     exampleSnapshot1,
 	},
 	// Uses non existing snapshot to copy from
 	{
-		name:          "Uses non existing snapshot to copy from",
-		identifier:    "test1",
-		instanceType:  "db.m1.small",
-		user:          "master",
-		password:      "master",
-		size:          6144,
-		from:          "develop",
-		expected:      "",
-		expectedError: "Couldn't find snapshot for develop instance",
-		snapshot:      exampleSnapshot1,
+		testCase: testCase{
+			expected:      "",
+			expectedError: "No snapshot found for develop instance",
+		},
+		name:         "Uses non existing snapshot to copy from",
+		identifier:   "test1",
+		instanceType: "db.m1.small",
+		user:         "master",
+		password:     "master",
+		size:         6144,
+		from:         "develop",
+		snapshot:     exampleSnapshot1,
 	},
 }
 
@@ -81,28 +79,7 @@ func TestCloneInstance(t *testing.T) {
 					params,
 					svc,
 				)
-				switch {
-				case err != nil && test.expectingError(err):
-					t.Errorf(
-						"Unexpected error: %v",
-						err,
-					)
-				case err == nil && test.expectedError != "":
-					t.Errorf(
-						"Expected error: %v missing",
-						test.expectedError,
-					)
-				case err == nil:
-					if diff := deep.Equal(
-						actual,
-						test.expected,
-					); diff != nil {
-						t.Errorf(
-							"Unexpected output: %s",
-							diff,
-						)
-					}
-				}
+				test.check(actual, err, t)
 			},
 		)
 	}
