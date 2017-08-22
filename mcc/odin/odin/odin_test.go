446cfbf900e16767fbe420939e532caa4caefac0
Ignasi Fosch <natx@y10k.ws>
2017-08-22 12:09:53 +0200

    Move snapshot example for tests to snapshot_list_test.go

--- mcc/odin/odin/odin_test.go
+++ mcc/odin/odin/odin_test.go
@@ -3,7 +3,6 @@ package odin_test
 import (
 	"testing"
 
-	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/rds"
 	"github.com/go-test/deep"
 
@@ -42,19 +41,6 @@ func (tc *testCase) check(actual interface{}, err error, t *testing.T) {
 			)
 		}
 	}
-}
-
-var exampleSnapshot1Type = aws.String("db.m1.medium")
-var exampleSnapshot1DBID = aws.String("production-rds")
-var exampleSnapshot1ID = aws.String("rds:production-2015-06-11")
-
-var exampleSnapshot1 = &rds.DBSnapshot{
-	AllocatedStorage:     aws.Int64(10),
-	AvailabilityZone:     aws.String("us-east-1c"),
-	DBInstanceIdentifier: exampleSnapshot1DBID,
-	DBSnapshotIdentifier: exampleSnapshot1ID,
-	MasterUsername:       aws.String("owner"),
-	Status:               aws.String("available"),
 }
 
 type getLastSnapshotCase struct {
---
+++ mcc/odin/odin/snapshot_list_test.go
@@ -0,0 +1,19 @@
+package odin_test
+
+import (
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/rds"
+)
+
+var exampleSnapshot1Type = aws.String("db.m1.medium")
+var exampleSnapshot1DBID = aws.String("production-rds")
+var exampleSnapshot1ID = aws.String("rds:production-2015-06-11")
+
+var exampleSnapshot1 = &rds.DBSnapshot{
+	AllocatedStorage:     aws.Int64(10),
+	AvailabilityZone:     aws.String("us-east-1c"),
+	DBInstanceIdentifier: exampleSnapshot1DBID,
+	DBSnapshotIdentifier: exampleSnapshot1ID,
+	MasterUsername:       aws.String("owner"),
+	Status:               aws.String("available"),
+}
