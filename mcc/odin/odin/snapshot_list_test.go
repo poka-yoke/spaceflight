6b855328fdbad679c8c33460eca35ff7380e5ea7
Ignasi Fosch <natx@y10k.ws>
2017-08-22 16:12:00 +0200

    Add empty ListSnapshots

--- mcc/odin/odin/snapshot_list_test.go
+++ mcc/odin/odin/snapshot_list_test.go
@@ -1,8 +1,12 @@
 package odin_test
 
 import (
+	"testing"
+
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/rds"
+
+	"github.com/poka-yoke/spaceflight/mcc/odin/odin"
 )
 
 var exampleSnapshot1Type = aws.String("db.m1.medium")
@@ -16,4 +20,40 @@ var exampleSnapshot1 = &rds.DBSnapshot{
 	DBSnapshotIdentifier: exampleSnapshot1ID,
 	MasterUsername:       aws.String("owner"),
 	Status:               aws.String("available"),
+}
+
+type listSnapshotsCase struct {
+	testCase
+	name       string
+	snapshots  []*rds.DBSnapshot
+	instanceID string
+}
+
+var listSnapshotsCases = []listSnapshotsCase{
+	// No snapshots for any instance
+	{
+		testCase: testCase{
+			expected:      []*rds.DBSnapshot{},
+			expectedError: "",
+		},
+		name:       "No snapshots for any instance",
+		snapshots:  []*rds.DBSnapshot{},
+		instanceID: "",
+	},
+}
+
+func TestListSnapshots(t *testing.T) {
+	svc := newMockRDSClient()
+	for _, test := range listSnapshotsCases {
+		t.Run(
+			test.name,
+			func(t *testing.T) {
+				actual, err := odin.ListSnapshots(
+					test.instanceID,
+					svc,
+				)
+				test.check(actual, err, t)
+			},
+		)
+	}
 }
