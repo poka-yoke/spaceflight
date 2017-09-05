67bff546732c240d66eea0d5c994a60ab9725ea5
Ignasi Fosch <natx@y10k.ws>
2017-09-05 13:03:26 +0200

    Add CLI wiring to odin for instance scale

---
+++ mcc/odin/cmd/instance_scale.go
@@ -0,0 +1,54 @@
+package cmd
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/spf13/cobra"
+
+	"github.com/poka-yoke/spaceflight/mcc/odin/odin"
+)
+
+// instanceScaleCmd represents the instance scale command
+var instanceScaleCmd = &cobra.Command{
+	Use:   "scale [flags] identifier",
+	Short: "Scales a database",
+	Long:  `Scales a database, according to defined attributes, in RDS.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		if len(args) != 1 {
+			log.Fatal(InstanceIDReq)
+		}
+		svc := odin.Init()
+		result, err := odin.ScaleInstance(
+			args[0],
+			instanceType,
+			svc,
+		)
+		if err != nil {
+			log.Fatalf("Error: %s", err)
+		}
+		fmt.Println(result)
+	},
+}
+
+func init() {
+	InstanceCmd.AddCommand(instanceScaleCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// createCmd.PersistentFlags().String("foo", "", "A help for foo")
+	instanceScaleCmd.PersistentFlags().StringVarP(
+		&instanceType,
+		"instance-type",
+		"t",
+		"db.m1.small",
+		"Instance type to use when creating DB Instance",
+	)
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// createCmd.Flags().BoolP("toggle", "t", false, "Toggle help message")
+
+}
