7e1d16e546507e6beea8b4b642a77012b3951a4f
Miguel Bernabeu <miguel.bernabeu@devex.com>
2017-11-07 17:04:04 +0100

    Move test tables into test functions

--- mcc/capcom/capcom/capcom_test.go
+++ mcc/capcom/capcom/capcom_test.go
@@ -7,14 +7,12 @@ import (
 	"testing"
 )
 
-var ListSecurityGroupsExpectedOutput = []string{
-	fmt.Sprintf("* %10s %20s %s\n", "sg-1234", "", ""),
-}
-
 func TestListSecurityGroups(t *testing.T) {
 	svc := &mockEC2Client{}
 	out := ListSecurityGroups(svc)
-	expected := ListSecurityGroupsExpectedOutput
+	expected := []string{
+		fmt.Sprintf("* %10s %20s %s\n", "sg-1234", "", ""),
+	}
 	for index, line := range out {
 		if line != expected[index] {
 			t.Error("Unexpected output")
@@ -22,46 +20,45 @@ func TestListSecurityGroups(t *testing.T) {
 	}
 }
 
-var biptable = []struct {
-	origin string
-	proto  string
-	port   int64
-	err    error
-}{
-	{
-		origin: "1.2.3.4/32",
-		proto:  "tcp",
-		port:   int64(0),
-		err:    nil,
-	},
-	{
-		origin: "sg-",
-		proto:  "tcp",
-		port:   int64(0),
-		err:    nil,
-	},
-	{
-		origin: "1.2.3.4/32",
-		proto:  "udp",
-		port:   int64(0),
-		err:    nil,
-	},
-	{
-		origin: "sg-",
-		proto:  "icmp",
-		port:   int64(0),
-		err:    nil,
-	},
-	{
-		origin: "1.2.3./32",
-		proto:  "udp",
-		port:   int64(0),
-		err:    errors.New(""),
-	},
-}
-
 func TestBuildIPPermission(t *testing.T) {
 	for _, tt := range biptable {
+	data := []struct {
+		origin string
+		proto  string
+		port   int64
+		err    error
+	}{
+		{
+			origin: "1.2.3.4/32",
+			proto:  "tcp",
+			port:   int64(0),
+			err:    nil,
+		},
+		{
+			origin: "sg-",
+			proto:  "tcp",
+			port:   int64(0),
+			err:    nil,
+		},
+		{
+			origin: "1.2.3.4/32",
+			proto:  "udp",
+			port:   int64(0),
+			err:    nil,
+		},
+		{
+			origin: "sg-",
+			proto:  "icmp",
+			port:   int64(0),
+			err:    nil,
+		},
+		{
+			origin: "1.2.3./32",
+			proto:  "udp",
+			port:   int64(0),
+			err:    errors.New(""),
+		},
+	}
 		_, err := BuildIPPermission(
 			tt.origin,
 			tt.proto,
@@ -74,27 +71,27 @@ func TestBuildIPPermission(t *testing.T) {
 	}
 }
 
-var csgtable = []struct {
-	name        string
-	description string
-	vpcid       string
-	out         string
-}{
-	{
-		name:        "",
-		description: "Non-VPC success",
-		vpcid:       "",
-		out:         "sg-12345678",
-	},
-	{
-		name:        "",
-		description: "VPC success",
-		vpcid:       "vpc-12345678",
-		out:         "sg-12345678",
-	},
-}
-
 func TestCreateSG(t *testing.T) {
+	data := []struct {
+		name        string
+		description string
+		vpcid       string
+		out         string
+	}{
+		{
+			name:        "",
+			description: "Non-VPC success",
+			vpcid:       "",
+			out:         "sg-12345678",
+		},
+		{
+			name:        "",
+			description: "VPC success",
+			vpcid:       "vpc-12345678",
+			out:         "sg-12345678",
+		},
+	}
+
 	svc := &mockEC2Client{}
 	for _, tt := range csgtable {
 		t.Run(
@@ -114,21 +111,20 @@ func TestCreateSG(t *testing.T) {
 	}
 }
 
-var fsgbntable = []struct {
-	name string
-	vpc  string
-	ret  []string
-}{
-	{
-		name: "",
-		vpc:  "",
-		ret: []string{
-			"sg-1234",
-		},
-	},
-}
-
 func TestFindSGByName(t *testing.T) {
+	data := []struct {
+		name string
+		vpc  string
+		ret  []string
+	}{
+		{
+			name: "",
+			vpc:  "",
+			ret: []string{
+				"sg-1234",
+			},
+		},
+	}
 	svc := &mockEC2Client{}
 	for _, tt := range fsgbntable {
 		ret := FindSGByName(tt.name, tt.vpc, svc)
@@ -140,25 +136,25 @@ func TestFindSGByName(t *testing.T) {
 	}
 }
 
-var fsgwtable = []struct {
-	cidr string
-	err  error
-	ret  []SearchResult
-}{
-	{
-		cidr: "1.2.3.4/32",
-		err:  nil,
-		ret: []SearchResult{
-			SearchResult{
-				GroupID:  "sg-1234",
-				Protocol: "tcp",
-				Port:     22,
-				Source:   "1.2.3.4/32",
-			}},
-	},
-}
-
 func TestFindSecurityGroupsWithRange(t *testing.T) {
+	data := []struct {
+		cidr string
+		err  error
+		ret  []SearchResult
+	}{
+		{
+			cidr: "1.2.3.4/32",
+			err:  nil,
+			ret: []SearchResult{
+				SearchResult{
+					GroupID:  "sg-1234",
+					Protocol: "tcp",
+					Port:     22,
+					Source:   "1.2.3.4/32",
+				}},
+		},
+	}
+
 	svc := &mockEC2Client{}
 	for _, tt := range fsgwtable {
 		ret, err := FindSecurityGroupsWithRange(svc, tt.cidr)
@@ -181,62 +177,61 @@ func TestFindSecurityGroupsWithRange(t *testing.T) {
 	}
 }
 
-var ncictable = []struct {
-	cidr string
-	ip   string
-	ret  bool
-	err  error
-}{
-	{
-		cidr: "0.0.0.0/0",
-		ip:   "1.2.3.4/32",
-		ret:  true,
-		err:  nil,
-	},
-	{
-		cidr: "deadbeef",
-		ip:   "1.2.3.4/32",
-		ret:  false,
-		err:  errors.New(""),
-	},
-	{
-		cidr: "192.168.1.0/24",
-		ip:   "192.168.1.1/32",
-		ret:  true,
-		err:  nil,
-	},
-	{
-		cidr: "192.168.1.0/24",
-		ip:   "192.168.3.1/32",
-		ret:  false,
-		err:  nil,
-	},
-	{
-		cidr: "192.168.1.0/24",
-		ip:   "192.168.1.0/24",
-		ret:  true,
-		err:  nil,
-	},
-	{
-		cidr: "192.168.1.0/24",
-		ip:   "192.168.3.0/24",
-		ret:  false,
-		err:  nil,
-	},
-	{
-		cidr: "192.168.0.0/16",
-		ip:   "192.168.10.0/24",
-		ret:  true,
-		err:  nil,
-	},
-}
-
 func TestNetworkContainsIPCheck(t *testing.T) {
 	for _, tt := range ncictable {
 		ip, _, _ := net.ParseCIDR(tt.ip)
 		ret, err := NetworkContainsIPCheck(tt.cidr, ip)
 		if (err != nil && tt.err == nil) ||
 			(err == nil && tt.err != nil) {
+	data := []struct {
+		cidr string
+		ip   string
+		ret  bool
+		err  error
+	}{
+		{
+			cidr: "0.0.0.0/0",
+			ip:   "1.2.3.4/32",
+			ret:  true,
+			err:  nil,
+		},
+		{
+			cidr: "deadbeef",
+			ip:   "1.2.3.4/32",
+			ret:  false,
+			err:  errors.New(""),
+		},
+		{
+			cidr: "192.168.1.0/24",
+			ip:   "192.168.1.1/32",
+			ret:  true,
+			err:  nil,
+		},
+		{
+			cidr: "192.168.1.0/24",
+			ip:   "192.168.3.1/32",
+			ret:  false,
+			err:  nil,
+		},
+		{
+			cidr: "192.168.1.0/24",
+			ip:   "192.168.1.0/24",
+			ret:  true,
+			err:  nil,
+		},
+		{
+			cidr: "192.168.1.0/24",
+			ip:   "192.168.3.0/24",
+			ret:  false,
+			err:  nil,
+		},
+		{
+			cidr: "192.168.0.0/16",
+			ip:   "192.168.10.0/24",
+			ret:  true,
+			err:  nil,
+		},
+	}
 			t.Error("Unexpected/mismatched error")
 		}
 		if ret != tt.ret {
