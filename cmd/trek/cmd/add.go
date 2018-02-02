b9aa938843ccddef1c1c9cb8d1ed8096f645a428
Miguel Bernabeu <miguel.bernabeu@devex.com>
2018-02-02 11:11:06 +0100

    Relocate trek command

--- cmd/trek/cmd/add.go
+++ cmd/trek/cmd/add.go
@@ -6,7 +6,7 @@ import (
 
 	"github.com/spf13/cobra"
 
-	"github.com/poka-yoke/spaceflight/mcc/trek/trek"
+	"github.com/poka-yoke/spaceflight/pkg/trek"
 )
 
 var original, final string
---
+++ cmd/trek/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/poka-yoke/spaceflight/cmd/trek/cmd"
+
+func main() {
+	cmd.Execute()
+}
