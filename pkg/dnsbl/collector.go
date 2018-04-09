23aba97d6570aa849101bcde57a57907897a2352
Miguel Bernabeu <miguel.bernabeu@devex.com>
2018-04-09 23:22:33 +0200

    DVX-6600 Add devex_exporter using dnsbl.Collector
    `dnsbl.Collector` is a Prometheus Collector compliant struct based off
    the `dnsbl.Checker` to collect all dnsbl metrics.
    
    `devex_exporter` is a Prometheus exporter that exposes
    `dnsbl.Collector` metrics for scraping. It's intended to be extended
    to provide other package metrics as well in the future.

---
+++ pkg/dnsbl/collector.go
@@ -0,0 +1,98 @@
+package dnsbl
+
+import (
+	"log"
+	"net"
+	"sync"
+
+	"github.com/prometheus/client_golang/prometheus"
+)
+
+// Collector implements a Prometheus Collector to report DNS blacklist
+// values
+type Collector struct {
+	providers               []string
+	positive, query, length prometheus.Gauge
+
+	// lookup contains the lookup function used
+	lookup func(string) ([]string, error)
+	// Functional control
+	wg sync.WaitGroup
+}
+
+// NewCollector creates a new, default configured Collector
+func NewCollector(providers []string) *Collector {
+	return &Collector{
+		lookup:    net.LookupHost,
+		providers: providers,
+		positive: prometheus.NewGauge(
+			prometheus.GaugeOpts{
+				Namespace:   "dnsbl",
+				Name:        "positive_count",
+				Help:        "Number of positive results of DNS blacklists",
+				ConstLabels: nil,
+			},
+		),
+		query: prometheus.NewGauge(
+			prometheus.GaugeOpts{
+				Namespace:   "dnsbl",
+				Name:        "query_count",
+				Help:        "Number of DNS blacklists providers that answered",
+				ConstLabels: nil,
+			},
+		),
+		length: prometheus.NewGauge(
+			prometheus.GaugeOpts{
+				Namespace:   "dnsbl",
+				Name:        "length_count",
+				Help:        "Number of DNS blacklists contacted",
+				ConstLabels: nil,
+			},
+		),
+	}
+}
+
+// Describe is a requirement for the Collector interface of Prometheus
+// that returns each exported metric's description to the Prometheus
+// middleware
+func (c *Collector) Describe(ch chan<- *prometheus.Desc) {
+	c.positive.Describe(ch)
+	c.query.Describe(ch)
+	c.length.Describe(ch)
+}
+
+// Collect is a requirement for the Collector interface of Prometheus
+// that runs the queries to set the metrics values to be exported
+func (c *Collector) Collect(ch chan<- prometheus.Metric) {
+	c.runCollection()
+
+	c.positive.Collect(ch)
+	c.query.Collect(ch)
+	c.length.Collect(ch)
+}
+
+func (c *Collector) runCollection() {
+	responses := make(chan int)
+	for _, provider := range c.providers {
+		c.wg.Add(1)
+		go func(provider string) {
+			result, _ := c.lookup(provider)
+			if len(result) > 0 {
+				log.Printf("%v returned %v\n", provider, result)
+			}
+			responses <- len(result)
+		}(provider)
+	}
+	go func() {
+		c.positive.Set(0)
+		c.query.Set(0)
+		for response := range responses {
+			c.positive.Add(float64(response))
+			c.query.Inc()
+			c.wg.Done()
+		}
+	}()
+	c.length.Set(float64(len(c.providers)))
+	c.wg.Wait()
+	close(responses)
+}
