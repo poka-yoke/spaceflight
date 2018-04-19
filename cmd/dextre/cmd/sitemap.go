db8eb88134654e1ed01f408d7df1836cc7535d32
Miguel Bernabeu <miguel.bernabeu@devex.com>
2018-04-19 15:37:19 +0200

    DVX-6950 Add dextre command for sitemaps
    This command uses the sitemap package to expose via Push Gateway a
    series of metrics over a list of publicly accessible sitemap files.

---
+++ cmd/dextre/cmd/sitemap.go
@@ -0,0 +1,79 @@
+package cmd
+
+import (
+	"bufio"
+	"log"
+	"os"
+
+	"github.com/prometheus/client_golang/prometheus/push"
+	"github.com/spf13/cobra"
+
+	"github.com/poka-yoke/spaceflight/pkg/sitemap"
+)
+
+var url, file string
+
+// sitemapCmd represents the sitemap command
+var sitemapCmd = &cobra.Command{
+	Use:   "sitemap",
+	Short: "Check for sitemap presence and timestamp",
+	Long:  ``,
+	Run: func(cmd *cobra.Command, args []string) {
+		var urls []string
+
+		if pgaddress == "" {
+			log.Fatal("pgaddress is mandatory")
+		}
+		if url == "" && file == "" {
+			log.Fatal("Either URL or file are mandatory")
+		}
+
+		if url != "" && file == "" {
+			urls = []string{url}
+		}
+		if file != "" {
+			fd, err := os.Open(file)
+			if err != nil {
+				log.Fatal("Couldn't open file ", file, err)
+			}
+			defer fd.Close()
+
+			scanner := bufio.NewScanner(fd)
+			for scanner.Scan() {
+				urls = append(urls, scanner.Text())
+			}
+		}
+
+		err := push.New(pgaddress, "sitemap").
+			Collector(sitemap.NewCollector(urls)).Push()
+		if err != nil {
+			log.Fatal("An error occurred while processing: ", err)
+		}
+	},
+}
+
+func init() {
+	RootCmd.AddCommand(sitemapCmd)
+
+	sitemapCmd.Flags().StringVarP(
+		&url,
+		"url",
+		"",
+		"",
+		"URL to fetch sitemap file.",
+	)
+	sitemapCmd.Flags().StringVarP(
+		&file,
+		"file",
+		"",
+		"",
+		"File contains the list of sitemap addresses to validate",
+	)
+	sitemapCmd.Flags().StringVarP(
+		&pgaddress,
+		"push-gateway",
+		"p",
+		"",
+		"Address of the Prometheus PushGateway to send results to.",
+	)
+}
