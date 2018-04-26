a56d5615d9cfdd9be321c59cb1a5d28285a6e7ac
Ignasi Fosch <natx@y10k.ws>
2018-04-26 16:06:29 +0200

    DVX-7220 Add package from cloudinary_exporter
    This adds the `cloudinary` package from
    [cloudinary_exporter](https://github.com/ifosch/cloudinary_exporter).
    The cloudinary_exporter is the one currently used to get usage metrics from
    cloudinary, but it is faulty due to how the Cloudinary's Admin API
    behaves.
    To fix that, this exporter is going to be replaced with a collector,
    written as a dextre subcommand, which will send metrics to Push
    Gateway.
    
    Refs [DVX-7220](https://mydevex.atlassian.net/browse/DVX-7220)

---
+++ pkg/cloudinary/usage_report.go
@@ -0,0 +1,128 @@
+package cloudinary
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+type UsageInfo struct {
+	Usage       int64   `json:"usage"`
+	Limit       int64   `json:"limit"`
+	UsedPercent float64 `json:"used_percent"`
+}
+
+type UsageReport struct {
+	Plan             string    `json:"plan"`
+	LastUpdate       string    `json:"last_updated"`
+	Transformations  UsageInfo `json:"transformations"`
+	Objects          UsageInfo `json:"objects"`
+	Bandwidth        UsageInfo `json:"bandwidth"`
+	Storage          UsageInfo `json:"storage"`
+	Requests         int64     `json:"requests"`
+	Resources        int64     `json:"resources"`
+	DerivedResources int64     `json:"derived_resources"`
+}
+
+func GetRequest() (req *http.Request, err error) {
+	cloudName, key, secret := CloudinaryCredentials.get()
+	req, err = http.NewRequest(
+		"GET",
+		fmt.Sprintf(
+			"https://%s:%s@api.cloudinary.com/v1_1/%s/usage",
+			key,
+			secret,
+			cloudName,
+		),
+		nil,
+	)
+	return req, err
+}
+
+func GetUsageReport(req *http.Request) (usageReport *UsageReport, err error) {
+	client := http.Client{}
+	rs, err := client.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf(
+			"ERROR: Request failure: %v",
+			err,
+		)
+	}
+	defer rs.Body.Close()
+
+	if rs.StatusCode != 200 && rs.StatusCode != 201 {
+		return nil, fmt.Errorf(
+			"ERROR: Cloudinary API complained: %v",
+			rs.Header.Get("X-Cld-Error"),
+		)
+	}
+	bodyBytes, err := ioutil.ReadAll(rs.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	usageReport = new(UsageReport)
+	err = json.Unmarshal(bodyBytes, &usageReport)
+	return usageReport, err
+}
+
+func DerivedResources(usageReport UsageReport) float64 {
+	return float64(usageReport.DerivedResources)
+}
+
+func Resources(usageReport UsageReport) float64 {
+	return float64(usageReport.Resources)
+}
+
+func Requests(usageReport UsageReport) float64 {
+	return float64(usageReport.Requests)
+}
+
+func StorageUsage(usageReport UsageReport) float64 {
+	return float64(usageReport.Storage.Usage)
+}
+
+func StorageLimit(usageReport UsageReport) float64 {
+	return float64(usageReport.Storage.Limit)
+}
+
+func StorageUsageRatio(usageReport UsageReport) float64 {
+	return float64(usageReport.Storage.UsedPercent / 100)
+}
+
+func BandwidthUsage(usageReport UsageReport) float64 {
+	return float64(usageReport.Bandwidth.Usage)
+}
+
+func BandwidthLimit(usageReport UsageReport) float64 {
+	return float64(usageReport.Bandwidth.Limit)
+}
+
+func BandwidthUsageRatio(usageReport UsageReport) float64 {
+	return float64(usageReport.Bandwidth.UsedPercent / 100)
+}
+
+func ObjectsUsage(usageReport UsageReport) float64 {
+	return float64(usageReport.Objects.Usage)
+}
+
+func ObjectsLimit(usageReport UsageReport) float64 {
+	return float64(usageReport.Objects.Limit)
+}
+
+func ObjectsUsageRatio(usageReport UsageReport) float64 {
+	return float64(usageReport.Objects.UsedPercent / 100)
+}
+
+func TransformationUsage(usageReport UsageReport) float64 {
+	return float64(usageReport.Transformations.Usage)
+}
+
+func TransformationLimit(usageReport UsageReport) float64 {
+	return float64(usageReport.Transformations.Limit)
+}
+
+func TransformationUsageRatio(usageReport UsageReport) float64 {
+	return float64(usageReport.Transformations.UsedPercent / 100)
+}
