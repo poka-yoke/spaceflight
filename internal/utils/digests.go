bfc46f4290b5aff1236a30f0fd32ec7589223d82
Ignasi Fosch <natx@y10k.ws>
2018-06-15 12:03:05 +0200

    DVX-7420 Add DigestContentBase64
    This adds a function to get Base64 digest of a io.Reader so it can be quickly
    compared to other ones.
    This function is using SHA1 for the digest.
    
    Refs [DVX-7420](https://mydevex.atlassian.net/browse/DVX-7420)

---
+++ internal/utils/digests.go
@@ -0,0 +1,17 @@
+package digests
+
+import (
+	"crypto/sha1"
+	"encoding/base64"
+	"io"
+)
+
+// ContentBase64 is providing a Base64 encoded SHA1 hash from a io.Reader.
+func ContentBase64(input io.Reader) (encoded string, err error) {
+	hasher := sha1.New()
+	if _, err = io.Copy(hasher, input); err != nil {
+		return "", err
+	}
+	encoded = base64.StdEncoding.EncodeToString(hasher.Sum(nil))
+	return encoded, nil
+}
