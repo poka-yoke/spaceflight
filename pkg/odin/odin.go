e5513d3818a194edad0994277291db5c950924cc
Miguel Bernabeu <miguel.bernabeu@devex.com>
2018-10-08 22:30:37 +0200

    odin: Remove WaitForInstance
    This is no longer used in `pkg/odin` and there is an equivalent
    function in `cmd/odin`.

--- pkg/odin/odin.go
+++ pkg/odin/odin.go
@@ -3,7 +3,6 @@ package odin
 import (
 	"fmt"
 	"sort"
-	"time"
 
 	"github.com/aws/aws-sdk-go/service/rds"
 	"github.com/aws/aws-sdk-go/service/rds/rdsiface"
@@ -13,9 +12,6 @@ const (
 	// RFC8601 is the date/time format used by AWS.
 	RFC8601 = "2006-01-02T15:04:05-07:00"
 )
-
-// Duration specified time to wait for instance to be available.
-var Duration = time.Duration(5) * time.Second
 
 // ModifiableParams is interface for params structs supporting
 // DBInstance modification.
@@ -67,30 +63,5 @@ func ListSnapshots(
 		},
 	)
 	result = output.DBSnapshots
-	return
-}
-
-// WaitForInstance waits until instance's status is "available".
-func WaitForInstance(
-	instance *rds.DBInstance,
-	svc rdsiface.RDSAPI,
-	status string,
-) (err error) {
-	var res *rds.DescribeDBInstancesOutput
-	for *instance.DBInstanceStatus != status {
-		id := instance.DBInstanceIdentifier
-		res, err = svc.DescribeDBInstances(
-			&rds.DescribeDBInstancesInput{
-				DBInstanceIdentifier: id,
-			},
-		)
-		if err != nil {
-			return
-		}
-		*instance = *res.DBInstances[0]
-		// This is to avoid AWS API rate throttling.
-		// Should use configurable exponential back-off
-		time.Sleep(Duration)
-	}
 	return
 }
