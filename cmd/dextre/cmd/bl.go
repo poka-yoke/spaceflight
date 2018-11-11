e7c94eae037c99121567e67424c6d04fd3e9174a
Miguel Bernabeu <miguel.bernabeu@devex.com>
2018-11-11 13:16:58 +0100

    Remove support for Nagios check
    We no longer need Nagios checks and its support was pretty bad. Let's
    remove it for now and we can reintroduce them proeprly if needed in
    the future.

--- cmd/dextre/cmd/bl.go
+++ cmd/dextre/cmd/bl.go
@@ -2,10 +2,8 @@ package cmd
 
 import (
 	"log"
-	"math"
 	"os"
 
-	"github.com/olorin/nagiosplugin"
 	"github.com/prometheus/client_golang/prometheus/push"
 	"github.com/spf13/cobra"
 
@@ -21,6 +19,13 @@ var blCmd = &cobra.Command{
 	Short: "Check for blacklist presence",
 	Long:  `Checks the supplied list of DNS-based blacklists for a specific IP presence. It returns the results in a Nagios compliat string. Thresholds for different Nagios states can be supplied as well.`,
 	Run: func(cmd *cobra.Command, args []string) {
+		if blacklist == "" {
+			log.Fatal("No file with blacklist addresses specified")
+		}
+		if pgaddress == "" {
+			log.Fatal("Push Gateway address is required")
+		}
+
 		blfile, err := os.Open(blacklist)
 		if err != nil {
 			log.Fatal("Could't open file ", blacklist, err)
@@ -28,48 +33,12 @@ var blCmd = &cobra.Command{
 		defer blfile.Close()
 
 		providers := dnsbl.GetProviders(ipAddress, blfile)
-		if pgaddress == "" {
-			positive, queried, length := dnsbl.NewChecker(providers).Query().Stats()
 
-			check := nagiosplugin.NewCheck()
-			defer check.Finish()
-			must(check.AddPerfDatum("queried", "", float64(queried), 0.0, math.Inf(1)))
-			must(check.AddPerfDatum("positive", "", float64(positive), 0.0, math.Inf(1)))
-			check.AddResultf(
-				nagiosplugin.OK,
-				"%v present in %v(%v%%) out of %v BLs",
-				ipAddress,
-				positive,
-				positive*100/length,
-				length,
-			)
-			switch {
-			case positive > length*warning/100:
-				check.AddResultf(
-					nagiosplugin.WARNING,
-					"%v present in %v(%v%%) out of %v BLs",
-					ipAddress,
-					positive,
-					positive*100/length,
-					length,
-				)
-			case positive > length*critical/100:
-				check.AddResultf(
-					nagiosplugin.CRITICAL,
-					"%v present in %v(%v%%) out of %v BLs",
-					ipAddress,
-					positive,
-					positive*100/length,
-					length,
-				)
-			}
-		} else {
-			err := push.New(pgaddress, "dnsbl").
-				Collector(dnsbl.NewCollector(providers)).Push()
+		err = push.New(pgaddress, "dnsbl").
+			Collector(dnsbl.NewCollector(providers)).Push()
 
-			if err != nil {
-				log.Fatal("Could not push completion time to Pushgateway: ", err)
-			}
+		if err != nil {
+			log.Fatal("Could not push completion time to Pushgateway: ", err)
 		}
 	},
 }
@@ -79,8 +48,6 @@ func init() {
 
 	blCmd.Flags().StringVarP(&ipAddress, "ip", "", "127.0.0.1", "IP Address to look for in the BLs")
 	blCmd.Flags().StringVarP(&blacklist, "file", "f", "", "Path to file containing black list addresses")
-	blCmd.Flags().IntVarP(&warning, "warning", "w", 90, "IP Address to look for in the BLs")
-	blCmd.Flags().IntVarP(&critical, "critical", "c", 95, "Path to file containing black list addresses")
 	blCmd.Flags().StringVarP(
 		&pgaddress,
 		"push-gateway",
